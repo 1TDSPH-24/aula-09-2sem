7ff0dbcd71199b382fb76b3ecec93dddd74373e9
gustavodscruz <gustavodiasdsc@gmail.com>
2024-09-09 12:07:33 -0300

    algo

--- aula09/src/routes/Home/index.tsx
+++ aula09/src/routes/Home/index.tsx
@@ -1,46 +1,62 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+type pokemon = {
+    name : string,
+    imagem : string
+}
+
+
 export default function Home() {
 
-    const [users, setUsers] = useState([{
-        id: 0,
-        login:"",
-        avatar_url:""
-    }]);
+    const [pokemons, setPokemons] = useState({
+        name: "",
+        imagem: ""
+    });
     const [error, setError] = useState<string | null>(null)
 
     //Criando o navigate para redirecionar para uma rota devida.
     const navigate = useNavigate();
 
     useEffect(() => {
+        const pegarPokemon = async (id: Number) => {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            try {
+                if (!response.ok) {
+                    throw new Error("Dados solicitados incompletos")
+                }
 
-        fetch('https://api.github.com/users')
-            .then(response => {
-                if (!response.ok) throw new Error('Erro ao gerar os dados');
-                return response.json();
-            })
-            .then(data => {
-                setUsers(data) 
-            })
-            .catch(err => {
-                setError(err.message);
-                navigate('/error');
-            })
+                const data = await response.json();
+                console.log(data)
+                const name = data['name'];
+                const imagem = data['sprites']['front_default']
+                const pokemon = {
+                    name : name,
+                    imagem : imagem
+                }
+                setPokemons(pokemon)
 
-    });  
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        pegarPokemon(1)
+
+    });
 
     return (
         <main>
             <h1>Welcome to my home page</h1>
-                <div>
-                    {users.map(user => (
-                        <div key={user.id}>
-                            <img src={user.avatar_url} alt={user.login} />
-                            <p>{user.login}</p>
-                        </div>
-                        ))}
-                </div>
+            <div>
+                {/* {pokemons.map((pokemon, indice) => (
+                    <div key={indice}>
+                        <p>{pokemon.name}</p>
+                        <img src={pokemon.imagem} />
+                    </div>
+                ))} */}
+                <p>{pokemons.name}</p>
+                <img src={pokemons.imagem} alt="" />
+            </div>
         </main>
     );
 }
